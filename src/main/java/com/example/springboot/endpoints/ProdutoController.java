1358f3a0cf36d7ca3eee7ef92dbee1d6919b0f35
Fabricio Oliveira <o>
2021-07-19 17:32:21 -0300

    Definindo a documentação da API com o Swagger 2.

--- src/main/java/com/example/springboot/endpoints/ProdutoController.java
+++ src/main/java/com/example/springboot/endpoints/ProdutoController.java
@@ -2,6 +2,9 @@ package com.example.springboot.endpoints;
 
 import com.example.springboot.entity.ProdutoEntity;
 import com.example.springboot.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,41 +19,55 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/produtos")
+@Api(tags = {"Spring Boot REST API de Produtos"})
 public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
     @GetMapping
+    @ApiOperation("Retorna uma lista com todos os produtos.")
     public ResponseEntity<List<ProdutoEntity>> getAllProdutos() {
         List<ProdutoEntity> produtos = produtoRepository.findAll();
         if(produtos.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+
         produtos.forEach(produto -> {
             Long id = produto.getId();
             produto.add(linkTo(methodOn(ProdutoController.class).getProduto(id)).withSelfRel());
         });
+
         return new ResponseEntity<List<ProdutoEntity>>(produtos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProdutoEntity> getProduto(@PathVariable("id") Long id) {
+    @ApiOperation("Retorna um produto, de acordo com o ID informado.")
+    public ResponseEntity<ProdutoEntity> getProduto(
+            @ApiParam("ID do Produto do tipo Long")
+            @PathVariable("id") Long id) {
         Optional<ProdutoEntity> produto = produtoRepository.findById(id);
         if(!produto.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+
         produto.get().add(linkTo(methodOn(ProdutoController.class).getAllProdutos()).withRel("Lista de Produtos"));
         return new ResponseEntity<ProdutoEntity>(produto.get(), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<ProdutoEntity> saveProduto(@RequestBody @Valid ProdutoEntity produto) {
+    @ApiOperation("Cria um novo Produto no banco de dados")
+    public ResponseEntity<ProdutoEntity> saveProduto(
+            @ApiParam("Representação do objeto do tipo Produto")
+            @RequestBody @Valid ProdutoEntity produto) {
         return new ResponseEntity<ProdutoEntity>(produtoRepository.save(produto), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ProdutoEntity> deleteProduto(@PathVariable("id") Long id) {
+    @ApiOperation("Deleta um Produto do banco de dados")
+    public ResponseEntity<ProdutoEntity> deleteProduto(
+            @ApiParam("Id do Produto do tipo Long")
+            @PathVariable("id") Long id) {
         Optional<ProdutoEntity> produto = produtoRepository.findById(id);
         if(!produto.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -60,7 +77,12 @@ public class ProdutoController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProdutoEntity> updateProduto(@PathVariable("id") Long id, @RequestBody ProdutoEntity produto) {
+    @ApiOperation("Deleta um Produto do banco de dados")
+    public ResponseEntity<ProdutoEntity> updateProduto(
+            @ApiParam("Id do Produto do tipo Long")
+            @PathVariable("id") Long id,
+            @ApiParam("Representação do objeto do tipo Produto")
+            @RequestBody ProdutoEntity produto) {
         Optional<ProdutoEntity> produtoOptional = produtoRepository.findById(id);
         if(!produtoOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
